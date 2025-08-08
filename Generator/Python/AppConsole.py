8056573efe65e01ca4daaf658ebe4eddf37150fd
StefanoP85 <85745378+StefanoP85@users.noreply.github.com>
2025-08-08 22:53:23 +0200

    Create AppConsole.py
    Added a python version

---
+++ Generator/Python/AppConsole.py
@@ -0,0 +1,12 @@
+import sys
+import Archives
+import Builder
+
+Archive = Archives.TArchive()
+if Archive.Load("Arena.pk3"):
+	Map = Archive.MapDefinitions[0]
+	NavMeshSettings = Builder.TNavMeshSettings(64, 32)
+	NavMesh = Builder.TNavMesh()
+	NavMesh.Build(NavMeshSettings, Map)
+	print(NavMesh, file = sys.stdout)
+	print(NavMesh.GetMessages(), file = sys.stderr)
